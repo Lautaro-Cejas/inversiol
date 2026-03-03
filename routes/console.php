373d39c90eee86a54093b2288a2ef57913b869f4
Lautaro Cejas <lauti217@live.com>
2026-03-03 02:19:56 +0000

    feat: update broker:trade command frequency to every five minutes

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('broker:sync')->everyFifteenMinutes()->weekdays()->between('11:00', '17:00');
 Schedule::command('broker:cazar')->everyFiveMinutes()->weekdays()->between('11:00', '17:00');
-Schedule::command('broker:trade --execute')->everyThirtyMinutes()->weekdays()->between('11:00', '17:00');
+Schedule::command('broker:trade --execute')->everyFiveMinutes()->weekdays()->between('11:00', '17:00');
